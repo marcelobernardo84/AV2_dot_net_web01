53f4a7ed9aec05ada3858d83ad009501744595ef
Marcelo Bernardo <marcelobernardo84@gmail.com>
2016-06-11 02:55:16 -0300

    Configuraçao do Entity

---
+++ CadastroProdutos_/DAL/ControleDePedidosDbContext.cs
@@ -0,0 +1,23 @@
+﻿using CadastroProdutos_.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace CadastroProdutos_.DAL
+{
+    public class ControleDePedidosDbContext : DbContext
+    {
+        public ControleDePedidosDbContext() : base("ControleDePedidosDbContext") { }
+        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Pedido> Pedidos { get; set; }
+        public DbSet<ItemPedido> ItemPedidos { get; set; }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
---
+++ CadastroProdutos_/DAL/ControleDePedidosDbInitialize.cs
@@ -0,0 +1,26 @@
+﻿using CadastroProdutos_.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CadastroProdutos_.DAL
+{
+    public class ControleDePedidosDbInitialize : System.Data.Entity.
+        DropCreateDatabaseIfModelChanges<ControleDePedidosDbContext>
+    {
+        protected override void Seed(ControleDePedidosDbContext context)
+        {
+            var produtos = new List<Produto>
+            {
+                new Produto {Valor=15,Nome="Chinelo", Marca="Havaianas", Estoque=20 },
+                new Produto {Valor=25,Nome="Bola de futebol", Marca="nike", Estoque=30 },
+                new Produto {Valor=60,Nome="Bola de basquete", Marca="adidas", Estoque=50 },
+                new Produto {Valor=30,Nome="Bola de Volei", Marca="rebook", Estoque=10 }
+            };
+
+            produtos.ForEach(s => context.Produtos.Add(s));
+            context.SaveChanges();
+        }
+    }
+}
